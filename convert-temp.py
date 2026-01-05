5737f163da20f1ae195a19ee70dbd8c0d302457e
Immanuel Bintang <106735816+immanuelbint@users.noreply.github.com>
2026-01-05 14:43:09 +0700

    Add simple temperature conversion from Celsius to Fahrenheit using python

---
+++ convert-temp.py
@@ -0,0 +1,7 @@
+# Simple Python Program to convert celsius to fahrenheit
+
+celsius = 37 # change this value
+
+# calculate fahrenheit temperature
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f Celsius is equal to %0.1f Fahrenheit' %(celsius,fahrenheit))
